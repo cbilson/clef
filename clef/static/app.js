34e93acfed41fdfbb64919335444f06a509e83e0
Chris Bilson <cbilson@microsoft.com>
2018-04-11 10:12:02 -0700

    cancel pending search requests

--- clef/static/app.js
+++ clef/static/app.js
@@ -1,26 +1,31 @@
 M.Dropdown.init(document.querySelector('.dropdown-trigger'));
 
 searchTextOptions = {minLength: 3};
-search = M.Autocomplete.init(document.getElementById('search-text'), searchTextOptions);
-search.el.addEventListener('input', function(e) {
-  if (e.target.value.length < 3) {
-    return;
-  }
+search = M.Autocomplete.init(
+  document.getElementById('search-text'),
+  searchTextOptions);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/search?term=' + e.target.value);
-  xhr.onload = function() {
-    console.log(xhr.responseText);
-    if (xhr.status == 200) {
+var pendingSearch = null;
+search.el.addEventListener('input', function(e) {
+  if (e.target.value.length < 3)
+    return;
+
+  if (pendingSearch != null)
+    pendingSearch.abort();
+
+  pendingSearch = new XMLHttpRequest();
+  pendingSearch.open('GET', '/search?term=' + e.target.value);
+  pendingSearch.onload = function() {
+    if (pendingSearch.status == 200) {
       data = {};
-      var results = JSON.parse(xhr.responseText);
-      for (var i = 0; i < results.length; i++) {
+      var results = JSON.parse(pendingSearch.responseText);
+      for (var i = 0; i < results.length; i++)
         data[results[i].name + ' (' + results[i].type + ')'] = null;
-      }
 
       search.updateData(data);
+      pendingSearch = null;
     }
   }
 
-  xhr.send();
+  pendingSearch.send();
 });
