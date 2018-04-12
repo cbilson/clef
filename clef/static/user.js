1b3af7895fda9dba4d455adcd7858cbc6b565c44
Chris Bilson <cbilson@microsoft.com>
2018-04-11 19:25:05 -0700

    show playlist details attributes on the playlist details page

--- clef/static/user.js
+++ clef/static/user.js
@@ -6,10 +6,10 @@ function logFailed(req) {
   console.log("Request failed: " + req);
 }
 
-function startBackgroundOperation(button, spinner, url, data, success, failed, verb) {
+function startBackgroundOperation(button, feedback, url, data, success, failed, verb) {
   if (button.classList.contains('disabled')) { return; }
   button.classList.add('disabled');
-  spinner.classList.add('busy');
+  feedback.classList.add('busy');
   var xhr = new XMLHttpRequest();
   verb = verb == null ? 'POST' : verb;
   xhr.open(verb, url);
@@ -17,7 +17,7 @@ function startBackgroundOperation(button, spinner, url, data, success, failed, v
     xhr.setRequestHeader('Content-Type', 'application/json');
 
   xhr.onload = function() {
-    spinner.classList.remove('busy');
+    feedback.classList.remove('busy');
     if (xhr.status == 200) {
       if (success != null)
         success(JSON.parse(xhr.responseText))
@@ -66,15 +66,64 @@ function mouseLeavePlaylistItem(item) {
   //   playlistCover.setAttribute('src', '');
 }
 
+function showGenres(playlistDetails) {
+  var genreCloud = document.getElementById('genre-cloud');
+  while (genreCloud.firstChild)
+    genreCloud.removeChild(genreCloud.firstChild);
+
+  var maxScore = 0;
+  for (var genre in playlistDetails.genres)
+    if (playlistDetails.genres[genre] > maxScore)
+      maxScore = playlistDetails.genres[genre]
+
+  console.log('max genre score: ' + maxScore);
+
+  for (var genre in playlistDetails.genres) {
+    var genreSpan = document.createElement('span');
+    genreSpan.innerText = genre;
+    var score = playlistDetails.genres[genre];
+    var pct = score / maxScore;
+    console.log('score for ' + genre + ' = ' + score + ' (pct. ' + pct + ')');
+    genreSpan.style.fontSize = (100 * pct) + '%';
+    genreSpan.style.margin = '3px';
+    genreCloud.appendChild(genreSpan);
+  }
+}
+
+function showPlaylistAttributes(playlistDetails) {
+  document.getElementById('danceability-value').innerText = playlistDetails.danceability.toFixed(2);
+  document.getElementById('energy-value').innerText = playlistDetails.energy.toFixed(2);
+  document.getElementById('loudness-value').innerText = playlistDetails.loudness.toFixed(2);
+  document.getElementById('tempo-value').innerText = playlistDetails.tempo.toFixed(0);
+  document.getElementById('acousticness-value').innerText = playlistDetails.acousticness.toFixed(2);
+  document.getElementById('instrumentalness-value').innerText = playlistDetails.instrumentalness.toFixed(2);
+  document.getElementById('liveness-value').innerText = playlistDetails.liveness.toFixed(2);
+  document.getElementById('speechiness-value').innerText = playlistDetails.speechiness.toFixed(2);
+  document.getElementById('valence-value').innerText = playlistDetails.valence.toFixed(2);
+}
+
 function clickPlaylist(item) {
   var url = 'playlist/' + item.target.dataset.id + '/details';
   var icon = document.getElementById('playlist-loading');
+  icon.classList.remove('hide');
   startBackgroundOperation(
     userSaveButton, icon, url, null,
-    function() {
-      console.log('loaded playlist.');
+    function(playlistDetails) {
+      icon.classList.add('hide');
+      var autoPlay = document.getElementById('auto-play');
+      if (autoPlay.value) {
+        var nowPlayling = document.getElementById('now-playing');
+        nowPlayling.classList.remove('hide');
+
+        var nowPlayingName = document.getElementById('now-playing-name');
+        nowPlayingName.innerText = playlistDetails.name;
+
+        showGenres(playlistDetails);
+        showPlaylistAttributes(playlistDetails);
+      }
     },
     function(xhr) {
+      icon.classList.add('hide');
       showPageWarning("Failed to load playlist.");
     }, 'GET');
 }
@@ -122,4 +171,3 @@ userSaveButton.addEventListener('click', function(e) {
       showPageWarning("Failed to save your changes: " + xhr.responseText);
     });
 });
-
